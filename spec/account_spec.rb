56c0ccf19f236c67ba87fea9833760c654cddd17
Daniel Whitston <dan@whitston.org.uk>
2017-03-09 10:57:17 +0000

    balance and deposit methods in Account

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,7 +1,20 @@
 require 'account'
 
 describe Account do
-  it 'starts with a balance of zero' do
-    expect(subject.balance).to eq 0
+  describe '#balance' do
+    it 'starts with a balance of zero' do
+      expect(subject.balance).to eq 0
+    end
+  end
+
+  describe '#deposit' do
+    it 'responds to deposit' do
+      expect{ subject.deposit(10)}.not_to raise_error
+    end
+
+    it "shows a balance of 10 after depositing 10" do
+      subject.deposit(10)
+      expect(subject.balance).to eq 10
+    end
   end
 end
